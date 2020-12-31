a8d61b9c4b226348f6acf49c099cafb45a65baed
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2020-12-30 19:58:04 -0700

    Finished homepage contact section

--- public/js/app.js
+++ public/js/app.js
@@ -9680,6 +9680,25 @@ var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
 
 /***/ }),
 
+/***/ "./node_modules/css-loader/index.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./resources/sass/Contact.scss":
+/*!************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./resources/sass/Contact.scss ***!
+  \************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__(/*! ../../node_modules/css-loader/lib/css-base.js */ "./node_modules/css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, ":root {\n  --transparency: 1;\n}\n\n.contact__card {\n  margin-bottom: 4rem;\n  padding: 3rem 7rem;\n  background-color: rgba(0, 56, 168, 0.07);\n  border-radius: 20px;\n  -webkit-border-radius: 20px;\n  -moz-border-radius: 20px;\n  -ms-border-radius: 20px;\n  -o-border-radius: 20px;\n}\n\n.contact__card__info--title {\n  margin-bottom: 1rem;\n}\n\n.contact__card__info--text {\n  text-align: center;\n}\n\n.contact__card__social--title,\n.contact__card__social--phone {\n  margin-bottom: 2rem;\n}\n\n.contact__card__social--email {\n  color: #000000;\n  font-size: 1.5rem;\n  font-weight: 500;\n  text-decoration: underline;\n  transition: opacity 0.3s ease-in-out;\n  -webkit-transition: opacity 0.3s ease-in-out;\n  -moz-transition: opacity 0.3s ease-in-out;\n  -ms-transition: opacity 0.3s ease-in-out;\n  -o-transition: opacity 0.3s ease-in-out;\n}\n\n.contact__card__social--email:hover {\n  color: #000000;\n  opacity: 0.5;\n}\n\n.contact__card__social--phone {\n  margin-top: 0.5rem;\n  font-weight: 500;\n}\n\n.contact__card__prof--github,\n.contact__card__prof--linkedin {\n  --transparency: 0.07;\n  width: 56px;\n  height: 56px;\n  border-radius: 100%;\n  -webkit-border-radius: 100%;\n  -moz-border-radius: 100%;\n  -ms-border-radius: 100%;\n  -o-border-radius: 100%;\n  transition: all 0.3s ease-in-out;\n  -webkit-transition: all 0.3s ease-in-out;\n  -moz-transition: all 0.3s ease-in-out;\n  -ms-transition: all 0.3s ease-in-out;\n  -o-transition: all 0.3s ease-in-out;\n  cursor: pointer;\n}\n\n.contact__card__prof--github .contact--github path {\n  transition: all 0.3s ease-in-out;\n  -webkit-transition: all 0.3s ease-in-out;\n  -moz-transition: all 0.3s ease-in-out;\n  -ms-transition: all 0.3s ease-in-out;\n  -o-transition: all 0.3s ease-in-out;\n}\n\n.contact__card__prof--github {\n  margin-right: 2rem;\n  background-color: rgba(51, 51, 51, var(--transparency));\n}\n\n.contact__card__prof--linkedin {\n  background-color: rgba(0, 119, 181, var(--transparency));\n}\n\n.contact__card__prof--github:hover,\n.contact__card__prof--linkedin:hover {\n  --transparency: 1;\n}\n\n.contact__card__prof--github:hover .contact--github path,\n.contact__card__prof--linkedin:hover .contact--linkedin path {\n  fill: #fbf9fa;\n}\n\n@media screen and (max-width: 991px) {\n  .contact__card__info {\n    margin-bottom: 2rem;\n  }\n}", ""]);
+
+// exports
+
+
+/***/ }),
+
 /***/ "./node_modules/css-loader/index.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./resources/sass/Header.scss":
 /*!***********************************************************************************************************************************************************!*\
   !*** ./node_modules/css-loader!./node_modules/postcss-loader/src??ref--7-2!./node_modules/sass-loader/dist/cjs.js??ref--7-3!./resources/sass/Header.scss ***!
@@ -86120,6 +86139,95 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/js/components/Contact.jsx":
+/*!*********************************************!*\
+  !*** ./resources/js/components/Contact.jsx ***!
+  \*********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_bootstrap__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-bootstrap */ "./node_modules/react-bootstrap/esm/index.js");
+/* harmony import */ var _sass_Contact_scss__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../../sass/Contact.scss */ "./resources/sass/Contact.scss");
+/* harmony import */ var _sass_Contact_scss__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_sass_Contact_scss__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+
+var Contact = function Contact() {
+  return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    id: "contact",
+    className: "contact"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Container"], null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Row"], {
+    className: "contact__card"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Col"], {
+    lg: 6,
+    xs: 12,
+    className: "contact__card__info d-flex flex-column align-items-center justify-content-center"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "contact__card__info--title h2"
+  }, "\xBFTe gust\xF3?"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "contact__card__info--text text"
+  }, "Juntos podemos hacer cosas similares e incluso m\xE1s grandiosas para ese proyecto que tienes en mente.")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Col"], {
+    lg: 6,
+    xs: 12,
+    className: "contact__card__btn d-flex flex-column align-items-center justify-content-center"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Button"], {
+    className: "contact__card__btn--btn",
+    href: "http://bit.ly/wp-juanda"
+  }, "Cont\xE1ctame"))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Row"], {
+    className: "contact__card__social d-flex flex-column align-items-center justify-content-center"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", {
+    className: "contact__card__social--title h3"
+  }, "Mantente conectado"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "contact__card__social--address text"
+  }, "Chihuahua, M\xE9xico"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "mailto:contacto@juandamartinez.com",
+    className: "contact__card__social--email"
+  }, "contacto@juandamartinez.com"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "contact__card__social--phone"
+  }, "(+52) 1 627 122 5229")), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Row"], {
+    className: "contact__card__prof"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_bootstrap__WEBPACK_IMPORTED_MODULE_1__["Col"], {
+    className: "d-flex justify-content-center"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "http://bit.ly/github-juanda",
+    className: "contact__card__prof--github d-flex justify-content-center align-items-center"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+    width: 29,
+    height: 29,
+    viewBox: "0 0 29 29",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg",
+    className: "contact--github"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+    d: "M17.9666 4.11821C15.6201 3.57305 13.1797 3.57305 10.8332 4.11824C9.47793 3.28633 8.44232 2.90459 7.68964 2.74374C7.28044 2.65629 6.95609 2.63445 6.71212 2.64097C6.59037 2.64422 6.48962 2.65451 6.40936 2.66686C6.36927 2.67303 6.33444 2.67969 6.30479 2.68619C6.28998 2.68944 6.27647 2.69264 6.26427 2.69572L6.24696 2.70024L6.23903 2.7024L6.23526 2.70346L6.23161 2.70449C6.23159 2.7045 6.23162 2.70449 6.23161 2.70449C6.23079 2.70487 6.23483 2.71577 6.49534 3.62668L6.23161 2.70449C5.95891 2.78248 5.73466 2.97706 5.61901 3.23603C5.29615 3.95898 5.11981 4.73876 5.10019 5.53029C5.0856 6.11849 5.15778 6.70451 5.31346 7.26965C4.90432 7.76524 4.57095 8.32019 4.32531 8.9165C4.00289 9.69921 3.83796 10.5378 3.83997 11.3843C3.84021 14.3652 4.71966 16.3695 6.21835 17.6573C7.26842 18.5596 8.54493 19.0397 9.82308 19.3146C9.61201 19.9011 9.55725 20.5215 9.59997 21.1536V22.3023C8.81786 22.4667 8.27779 22.4147 7.89278 22.2876C7.40897 22.128 7.03806 21.8068 6.68426 21.3456C6.50508 21.112 6.33999 20.8554 6.16665 20.5748L6.05659 20.3956C5.92083 20.174 5.77433 19.9348 5.62647 19.7167C5.26012 19.1765 4.71962 18.5013 3.84375 18.2714L2.91522 18.0276L2.42766 19.8846L3.35619 20.1284C3.50872 20.1685 3.70952 20.3108 4.03737 20.7943C4.15783 20.9719 4.27274 21.1595 4.40394 21.3736C4.44515 21.4408 4.48807 21.5108 4.53317 21.5839C4.71337 21.8756 4.92006 22.2003 5.16088 22.5142C5.64708 23.148 6.30907 23.7868 7.29106 24.1109C7.96575 24.3336 8.72838 24.3871 9.59997 24.2531V27.8399C9.59997 28.3701 10.0298 28.7999 10.56 28.7999H18.24C18.7702 28.7999 19.2 28.3701 19.2 27.8399V20.9999C19.2 20.3935 19.1736 19.8392 19.0029 19.3221C20.2746 19.0536 21.541 18.5731 22.5842 17.67C24.0824 16.3731 24.96 14.3508 24.96 11.3519L24.96 11.3488C24.9551 9.85356 24.4305 8.41327 23.4865 7.26968C23.6422 6.70453 23.7143 6.1185 23.6998 5.53029C23.6801 4.73876 23.5038 3.95898 23.1809 3.23603C23.0652 2.97698 22.8409 2.78237 22.5681 2.70443L22.3044 3.62749C22.5681 2.70443 22.5654 2.70365 22.5645 2.70339L22.5607 2.70234L22.5528 2.70018L22.5355 2.69567C22.5233 2.6926 22.5098 2.6894 22.495 2.68615C22.4653 2.67966 22.4305 2.67301 22.3904 2.66684C22.3102 2.6545 22.2094 2.64422 22.0876 2.64096C21.8436 2.63445 21.5192 2.65628 21.1099 2.74373C20.3571 2.90457 19.3214 3.28631 17.9666 4.11821Z",
+    fill: "#333333"
+  }))), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "http://bit.ly/linkedin-juanda",
+    className: "contact__card__prof--linkedin d-flex justify-content-center align-items-center"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
+    width: 29,
+    height: 29,
+    viewBox: "0 0 29 29",
+    fill: "none",
+    xmlns: "http://www.w3.org/2000/svg",
+    className: "contact--linkedin"
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
+    fillRule: "evenodd",
+    clipRule: "evenodd",
+    d: "M0 2.88C0 1.28942 1.28942 0 2.88 0H25.92C27.5106 0 28.8 1.28942 28.8 2.88V25.92C28.8 27.5106 27.5106 28.8 25.92 28.8H2.88C1.28942 28.8 0 27.5106 0 25.92V2.88ZM9.6 9.6H7.68V7.68H9.6V9.6ZM7.68 21.12V11.52H9.6V21.12H7.68ZM16.32 13.44C14.7294 13.44 13.44 14.7294 13.44 16.32V21.12H11.52V11.52H13.44V12.4796C14.2422 11.8771 15.2394 11.52 16.32 11.52C18.971 11.52 21.12 13.669 21.12 16.32V21.12H19.2V16.32C19.2 14.7294 17.9106 13.44 16.32 13.44Z",
+    fill: "#0077B5"
+  })))))));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Contact);
+
+/***/ }),
+
 /***/ "./resources/js/components/Header.jsx":
 /*!********************************************!*\
   !*** ./resources/js/components/Header.jsx ***!
@@ -86658,6 +86766,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_Hero__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../components/Hero */ "./resources/js/components/Hero.jsx");
 /* harmony import */ var _components_Skills__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../components/Skills */ "./resources/js/components/Skills.jsx");
 /* harmony import */ var _components_Projects__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../components/Projects */ "./resources/js/components/Projects.jsx");
+/* harmony import */ var _components_Contact__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../components/Contact */ "./resources/js/components/Contact.jsx");
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -86679,6 +86788,7 @@ function _assertThisInitialized(self) { if (self === void 0) { throw new Referen
 function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
 
 function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 
 
 
@@ -86710,7 +86820,7 @@ var Home = /*#__PURE__*/function (_Component) {
   }, {
     key: "render",
     value: function render() {
-      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Header__WEBPACK_IMPORTED_MODULE_2__["default"], null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Hero__WEBPACK_IMPORTED_MODULE_3__["default"], null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Skills__WEBPACK_IMPORTED_MODULE_4__["default"], null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Projects__WEBPACK_IMPORTED_MODULE_5__["default"], null));
+      return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react__WEBPACK_IMPORTED_MODULE_0___default.a.Fragment, null, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Header__WEBPACK_IMPORTED_MODULE_2__["default"], null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Hero__WEBPACK_IMPORTED_MODULE_3__["default"], null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Skills__WEBPACK_IMPORTED_MODULE_4__["default"], null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Projects__WEBPACK_IMPORTED_MODULE_5__["default"], null), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_components_Contact__WEBPACK_IMPORTED_MODULE_6__["default"], null));
     }
   }]);
 
@@ -86747,6 +86857,36 @@ var Router = function Router() {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Router);
+
+/***/ }),
+
+/***/ "./resources/sass/Contact.scss":
+/*!*************************************!*\
+  !*** ./resources/sass/Contact.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+var content = __webpack_require__(/*! !../../node_modules/css-loader!../../node_modules/postcss-loader/src??ref--7-2!../../node_modules/sass-loader/dist/cjs.js??ref--7-3!./Contact.scss */ "./node_modules/css-loader/index.js!./node_modules/postcss-loader/src/index.js?!./node_modules/sass-loader/dist/cjs.js?!./resources/sass/Contact.scss");
+
+if(typeof content === 'string') content = [[module.i, content, '']];
+
+var transform;
+var insertInto;
+
+
+
+var options = {"hmr":true}
+
+options.transform = transform
+options.insertInto = undefined;
+
+var update = __webpack_require__(/*! ../../node_modules/style-loader/lib/addStyles.js */ "./node_modules/style-loader/lib/addStyles.js")(content, options);
+
+if(content.locals) module.exports = content.locals;
+
+if(false) {}
 
 /***/ }),
 
--- resources/js/containers/Home.jsx
+++ resources/js/containers/Home.jsx
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import Hero from '../components/Hero';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
+import Contact from '../components/Contact';
 
 const TagManagerArgs = {
   gtmId: 'GTM-P9B2VRQ',
@@ -27,6 +28,8 @@ class Home extends Component {
         <Skills />
 
         <Projects />
+
+        <Contact />
       </>
     );
   }
