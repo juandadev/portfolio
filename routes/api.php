84137323a7955f2e74cf369311520a2bb4043ac8
Juan Daniel Martinez <mtzn_juandaniel@yahoo.com.mx>
2021-01-04 19:13:37 -0700

    Controller functions for Users. Auth API routes (login and register)

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'UserController@userSignUp');
+
+Route::post('login', 'UserController@userLogin');
+
+Route::get('user/{email}', 'UserController@userDetail');
